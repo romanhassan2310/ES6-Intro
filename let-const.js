940efae90ffce4598ee133542fe572b03f7dd351
Roman <romanhassan328@gmail.com>
2025-11-20 22:40:44 +0600

    let-const.js

---
+++ let-const.js
@@ -0,0 +1,26 @@
+// ES6
+// console.log(name)
+// console.log('device value:', device)
+// var device = 'Laptop'
+
+const name = 'Bangladesh';
+const countryName = 'Dhaka, '+ name;
+console.log(countryName);
+
+let price = 120;
+price =150
+let bookName = 'C++ Book'
+console.log(price,'tk', bookName);
+
+
+//
+const charge = 50;
+// charge = 45;
+
+const dress = ['shirt', 'pant', 'jacket', 'pen']
+dress.push ('T-shirt');
+console.log(dress)
+
+const student = {name: 'Roman', marks:70};
+student.marks = 89; 
+console.log(student)
